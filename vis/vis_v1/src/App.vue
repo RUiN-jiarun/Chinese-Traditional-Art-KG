a3ea761a950a340a4a171d42dda16873d3161e93
RUiN-jiarun <1052707477@qq.com>
2021-10-25 15:53:01 +0800

    10.25 tmp

--- vis/vis_v1/src/App.vue
+++ vis/vis_v1/src/App.vue
@@ -1,28 +1,90 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button class="setting-box-button" icon="el-icon-s-operation" circle @click="showSettingCard=!showSettingCard"></el-button>
+    <el-card class="setting-box-card" v-if="showSettingCard">
+      <div slot="header" class="clearfix">
+        <span>设置</span>
+        <el-button
+          style="float: right; padding: 3px 0"
+          type="text"
+          @click="showSettingCard=!showSettingCard"
+        >关闭</el-button>
+      </div>节点大小
+      <el-slider v-model="nodeSize" show-input :max="50"></el-slider>连线宽度
+      <el-slider v-model="linkWidth" show-input :max="20"></el-slider>连线长度
+      <el-slider v-model="linkDistance" show-input :max="300" :step="50" show-stops></el-slider>作用力
+      <el-slider v-model="bodyStrength" show-input :min="-1000" :max="0" :step="50" show-stops></el-slider>
+      <br>
+      <div>
+        <el-switch v-model="svgTheme" active-text="黑暗模式" inactive-text="明亮模式"></el-switch>
+      </div>
+    </el-card>
+    <network
+      :nodeList="nodes"
+      :linkList="links"
+      :nodeSize="nodeSize"
+      :linkWidth="linkWidth"
+      :linkDistance="linkDistance"
+      :svgTheme="svgTheme?'dark':'light'"
+      :bodyStrength="bodyStrength"
+    ></network>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Network from "./components/ForceDirectedGraph.vue";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    Network
+  },
+  data() {
+    return {
+      nodes: [],
+      links: [],
+      showSettingCard: false,
+      nodeSize: 14,
+      linkWidth: 2,
+      linkDistance: 50,
+      bodyStrength: -150,
+      svgTheme: false, // light
+    };
+  },
+  created() {
+    let url =
+      process.env.NODE_ENV === "production"
+        ? "/vue-network-d3/example.json"
+        : "/example.json";
+    axios
+      .get(url)
+      .then(response => {
+        this.nodes = response.data.nodes;
+        this.links = response.data.links;
+      })
+      .catch(err => console.log(err));
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+}
+.setting-box-button {
+  position: fixed;
+  overflow: auto;
+  top: 20px;
+  right: 40px;
+  border: 2px solid rebeccapurple;
+}
+.setting-box-card {
+  position: fixed;
+  z-index: 10;
+  overflow: auto;
+  top: 15px;
+  right: 15px;
+  width: 500px;
+  max-height: 700px;
 }
 </style>
